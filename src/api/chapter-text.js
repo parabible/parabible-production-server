ba68d4bad3e9969d68f4882bb7fb15b991a898be
jcuenod <j3frea+gitkraken@gmail.com>
2017-08-11 12:25:02 +0200

    Refactor variable names

--- src/api/chapter-text.js
+++ src/api/chapter-text.js
@@ -1,18 +1,19 @@
 import book_names from '../../data/book_names.json'
 
-const ridlistText = (ridlist, requested_texts_set, db) => {
+const ridlistText = (ridlist, requestedTextsSet, db) => {
 	return new Promise((resolve, reject) => {
 		let ridlistResponse = {}
 		const cursor = db.collection("verse_data").find({ rid: { $in: ridlist } })
-		console.log("busy processing cursor")
 		cursor.each((err, doc) => {
+			if (err)
+				console.log("ERROR", err)
 			if (doc != null) {
 				ridlistResponse[doc["rid"]] = {}
-				if (requested_texts_set.has("wlc"))
+				if (requestedTextsSet.has("wlc"))
 					ridlistResponse[doc["rid"]]["wlc"] = doc["wlc"]
-				if (requested_texts_set.has("net"))
+				if (requestedTextsSet.has("net"))
 					ridlistResponse[doc["rid"]]["net"] = doc["net"]
-				if (requested_texts_set.has("lxx"))
+				if (requestedTextsSet.has("lxx"))
 					ridlistResponse[doc["rid"]]["lxx"] = doc["lxx"]
 			} else {
 				resolve(ridlistResponse)
@@ -23,17 +24,18 @@ const ridlistText = (ridlist, requested_texts_set, db) => {
 }
 
 const chapterText = (params, db) => {
-	let requested_texts = new Set(params["texts"] || [])
-	if (!requested_texts.has("wlc") && 
-			!requested_texts.has("net") && 
-			!requested_texts.has("lxx"))
-		requested_texts.add("wlc")
+	let requestedTexts = new Set(params["texts"] || [])
+	console.log(requestedTexts)
+	if (!requestedTexts.has("wlc") && 
+			!requestedTexts.has("net") && 
+			!requestedTexts.has("lxx"))
+		requestedTexts.add("wlc")
 
 	const ref = params.reference
 	const minv = book_names[ref.book] * 10000000 + ref.chapter * 1000
 	const maxv = book_names[ref.book] * 10000000 + (ref.chapter+1) * 1000
 	return new Promise((resolve, reject) => {
-		ridlistText(Array.from({length: maxv-minv}, (v, k) => k+minv), requested_texts, db).then((texts) => {
+		ridlistText(Array.from({length: maxv-minv}, (v, k) => k+minv), requestedTexts, db).then((texts) => {
 			resolve({
 				"reference": params.reference,
 				"text": texts
