1b2e512bde3ee96f28a98a14b83b854ed270fa18
jcuenod <j3frea+gitkraken@gmail.com>
2017-11-24 13:00:20 -0600

    crazy improvements to efficiency of uniqueValuePerArray and general search results (should handle 100k+)

--- src/api/term-search.js
+++ src/api/term-search.js
@@ -9,7 +9,7 @@ import book_names from '../../data/book_names'
 
 const RESULT_LIMIT = 500
 
-const doLog = true
+const doLog = false
 const consoleLog = (...debug) => {
 	if (doLog) {
 		console.log(...debug)
@@ -57,7 +57,7 @@ const _queryForWids = ({queryArray, search_range, search_filter}) => {
 	let current_match = -1
 	let starttime = process.hrtime()
 	queryArray.forEach((query) => {
-		// consoleLog("BENCHMARK Q: foreach cycle ", process.hrtime(starttime))
+		consoleLog("BENCHMARK Q: foreach cycle ", process.hrtime(starttime))
 		const query_matches = _wordsThatMatchQuery(query.data, search_filter)
 
 		if (query.invert)
@@ -65,37 +65,49 @@ const _queryForWids = ({queryArray, search_range, search_filter}) => {
 		else
 			word_matches.push(query_matches)
 	})
-	// consoleLog("BENCHMARK Q: done with foreach", process.hrtime(starttime))
+	consoleLog("BENCHMARK Q: done with foreach", process.hrtime(starttime))
 
 	const sr_matches = word_matches.map(m => m.map(n => tree_data[n][search_range]))
 	const sr_exclusions = exclusions.map(m => tree_data[m][search_range])
 	const match_intersection = arrayIntersect(...sr_matches)
 	const range_matches = arrayDiff(match_intersection, sr_exclusions)
 
-	const range_matches_with_unique_limit = range_matches.map(r => {
-		const word_match_indices = sr_matches.map((q, qi) => q.reduce((a, e, i) => (e === r) ? a.concat(word_matches[qi][i]) : a, []))
-		const should_include = uniqueValuePerArray(word_match_indices) !== false ? word_match_indices : false
-		return {
-			sr_node: r,
-			matching_word_nodes: should_include
-		}
-	}).filter(m => m.matching_word_nodes !== false)
-	// consoleLog("BENCHMARK Q: done intersecting", process.hrtime(starttime))
-	// consoleLog("RESULTS:", range_matches_with_unique_limit.length)
+	consoleLog("BENCHMARK Q: done intersecting", process.hrtime(starttime))
+	const word_match_indices_map = word_matches.map(w => w.reduce((a,v) => {
+		if (!a.hasOwnProperty(tree_data[v][search_range]))
+			a[tree_data[v][search_range]] = []
+		a[tree_data[v][search_range]].push(v)
+		return a
+	}, {}))
+	const range_matches_with_unique_limit = range_matches.map((r, i) => {
+			const word_match_indices = word_match_indices_map.map((w, i) => w[r])
+			const should_include = uniqueValuePerArray(word_match_indices) ? word_match_indices : false
+			return {
+				sr_node: r,
+				matching_word_nodes: should_include
+			}
+		})
+		.filter(m => m && m.matching_word_nodes !== false)
+	consoleLog("BENCHMARK Q: query el indep. repr.", process.hrtime(starttime))
+	consoleLog("RESULTS:", range_matches_with_unique_limit.length)
 	return range_matches_with_unique_limit
-	// return { word_matches, range_matches }
 }
 
 const termSearch = (params, db)=> {
 	return new Promise((resolve, reject) => {
 		let starttime = process.hrtime()
-		// consoleLog("BENCHMARK: **querying for WIDS", process.hrtime(starttime))
+		consoleLog("BENCHMARK: **querying for WIDS", process.hrtime(starttime))
 		const matches = _queryForWids({
 			queryArray: params["query"],
 			search_range: params["search_range"] || "clause",
 			search_filter: params["search_filter"] || []
 		})
-		// consoleLog("BENCHMARK: **getting matching word sets", process.hrtime(starttime))
+		let truncated = false
+		if (matches.length > RESULT_LIMIT) {
+			truncated = matches.length
+			matches.splice(RESULT_LIMIT)
+		}
+		consoleLog("BENCHMARK: **getting matching word sets", process.hrtime(starttime))
 		const words_in_matching_ranges_set = new Set(matches.reduce((c, m) => c.concat(...range_node_data[m.sr_node]["wids"]), []))
 		const all_word_matches = matches.reduce((c,n) => c.concat(...n.matching_word_nodes), [])
 		const actual_matching_words_set = new Set(arrayIntersect(all_word_matches, words_in_matching_ranges_set))
@@ -107,7 +119,7 @@ const termSearch = (params, db)=> {
 		if (textsToReturn.length === 0)
 			textsToReturn = ["wlc", "net"]
 
-		// consoleLog("BENCHMARK: **now formulating final data", process.hrtime(starttime))
+		consoleLog("BENCHMARK: **now formulating final data", process.hrtime(starttime))
 		const ridmatches = matches.reduce((c, n) => c.concat(...range_node_data[n.sr_node]["rids"]), [])
 		ridlistText(ridmatches, new Set(textsToReturn), db).then((ridMatchText) => {
 			Object.keys(ridMatchText).forEach(rid => {
@@ -117,7 +129,7 @@ const termSearch = (params, db)=> {
 					words_in_matching_ranges_set
 				)
 			})
-			// consoleLog("BENCHMARK: **results now being processed", process.hrtime(starttime))
+			consoleLog("BENCHMARK: **results now being processed", process.hrtime(starttime))
 			const match_result_data = matches.map((m) => {
 				const ridTextObject = {}
 				range_node_data[m.sr_node]["rids"].forEach(rid => {
@@ -131,11 +143,11 @@ const termSearch = (params, db)=> {
 			})
 
 			const response = {
-				"length": match_result_data.length,
-				"results": match_result_data.slice(0, RESULT_LIMIT) // Don't kill the client
+				"truncated": truncated,
+				"results": match_result_data
 			}
 			resolve(response)
-			// consoleLog("BENCHMARK: **done", process.hrtime(starttime))
+			consoleLog("BENCHMARK: **done", process.hrtime(starttime))
 			consoleLog(`TermSearch: ${match_result_data.length} results (${process.hrtime(starttime)})`)
 		}).catch()
 	})
--- src/util/uniqueValuePerArray.js
+++ src/util/uniqueValuePerArray.js
@@ -8,21 +8,68 @@ const removeValue = (value) => {
 	}
 }
 
-const uniqueValuePerArray = (arrays) => {
+const fastConcatArrays = (arrays) => {
+	// Can be done like this: [].concat(...arrays) but it's slightly slower
+	let result = []
+	for (let i = 0; i < arrays.length; i++) {
+		const a = arrays[i]
+		for(var j = 0; j < a.length; j++)
+			result.push(a[j])
+	}
+	return result
+}
+const trimArrayBySize = (arraysSortedBySize) => {
+	// If arrays.length >= arrays[-1].length then arrays[-1]
+	// can satisfy the uniqueness requirement so we can ignore in tests
+	if (arraysSortedBySize.length === 0) {
+		return arraysSortedBySize
+	}
+	else if (arraysSortedBySize[arraysSortedBySize.length - 1].length >= arraysSortedBySize.length) {
+		arraysSortedBySize.pop()
+		return trimArrayBySize(arraysSortedBySize)
+	}
+	else {
+		return arraysSortedBySize
+	}
+}
+const bruteForceUniques = (arrays) => {
 	if (arrays.length === 1) {
-		return arrays[0].length > 0 ? arrays[0][0] : false
+		return arrays[0].length > 0
 	}
 	else {
 		const array_slice = arrays.slice(1)
 		for (let i in arrays[0]) {
 			const test_value = arrays[0][i]
 			const trial_array = array_slice.map(removeValue(test_value))
-			const trial = uniqueValuePerArray(trial_array)
+			const trial = bruteForceUniques(trial_array)
 			if (trial !== false) {
-				return test_value + " " + trial
+				return true
 			}
 		}
 		return false
+	}
+}
+const uniqueValuePerArray = (arrays) => {
+	if (arrays.length === 1) {
+		return arrays[0].length > 0
+	}
+	else {
+		const sortedArrays = arrays.slice().sort((a, b) => a.length > b.length)
+		const trimmedArrays = trimArrayBySize(sortedArrays)
+		if (trimmedArrays.length === 0) {
+			//The arrays have enough matches to guarantee a possible combination
+			return true
+		}
+		else if (trimmedArrays.length === 1) {
+			// Only one array remains - if this array has any values, it is possible, otherwise it's not - this array should definitely have a length > 0
+			return trimmedArrays[0].length > 0
+		}
+		// if the union of the arrays is not >= the number of arrays in the set,
+		// there's no way for each to have a unique value assigned
+		const uniqueSet = new Set(fastConcatArrays(trimmedArrays))
+		if (uniqueSet.size < trimmedArrays.length)
+			return false
+		return bruteForceUniques(trimmedArrays)
 	}
 }
 
